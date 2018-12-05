e12090699f43545d7adbe54a934f5de321e0421e
dawidkotarba <dawidkotarba>
2018-12-05 17:57:03 +0100

    Update controller test

--- src/test/groovy/com/dawidkotarba/backendtest/controller/TransferControllerSpec.groovy
+++ src/test/groovy/com/dawidkotarba/backendtest/controller/TransferControllerSpec.groovy
@@ -8,6 +8,7 @@ import groovy.json.JsonSlurper
 import io.micronaut.context.ApplicationContext
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.inject.qualifiers.Qualifiers
 import io.micronaut.runtime.server.EmbeddedServer
 import org.junit.Before
@@ -20,7 +21,7 @@ class TransferControllerSpec extends Specification {
     final static def TRANSFER_URL = "/api/transfer"
     final static def FIRST_TRANSFER_REQUEST_SEQUENCE_ID = 1L
     final static def SENDER_INITIAL_BALANCE = BigDecimal.TEN
-    final  def RECEIVER_INITIAL_BALANCE = BigDecimal.ONE
+    final static def RECEIVER_INITIAL_BALANCE = BigDecimal.ONE
 
     @Shared
     @AutoCleanup
@@ -41,10 +42,10 @@ class TransferControllerSpec extends Specification {
 
     @Before
     def setupAccounts() {
-        def senderAccount = accountRepository.save(new Account().withBalance(SENDER_INITIAL_BALANCE))
+        def senderAccount = accountRepository.save(new Account().withBalance(TransferControllerSpec.SENDER_INITIAL_BALANCE))
         senderAccountId = senderAccount.getId()
 
-        def receiverAccount = accountRepository.save(new Account().withBalance(RECEIVER_INITIAL_BALANCE))
+        def receiverAccount = accountRepository.save(new Account().withBalance(TransferControllerSpec.RECEIVER_INITIAL_BALANCE))
         receiverAccountId = receiverAccount.getId()
     }
 
@@ -53,12 +54,12 @@ class TransferControllerSpec extends Specification {
         def requestBody = createRequestBody(senderAccountId, receiverAccountId, BigDecimal.TEN)
 
         when:
-        def response = client.toBlocking().retrieve(HttpRequest.POST(TRANSFER_URL, requestBody))
+        def response = client.toBlocking().retrieve(HttpRequest.POST(TransferControllerSpec.TRANSFER_URL, requestBody))
         def parsedResponse = new JsonSlurper().parseText(response)
         def responseDto = new TransferResponseDto(parsedResponse)
 
         then:
-        responseDto.getTransferId() == FIRST_TRANSFER_REQUEST_SEQUENCE_ID
+        responseDto.getTransferId() == TransferControllerSpec.FIRST_TRANSFER_REQUEST_SEQUENCE_ID
     }
 
     def "Should subtract transfer amount from sender after a successful transaction"() {
@@ -68,10 +69,10 @@ class TransferControllerSpec extends Specification {
         def requestBody = createRequestBody(senderAccountId, receiverAccountId, transferAmount)
 
         when:
-        client.toBlocking().retrieve(HttpRequest.POST(TRANSFER_URL, requestBody))
+        client.toBlocking().retrieve(HttpRequest.POST(TransferControllerSpec.TRANSFER_URL, requestBody))
 
         then:
-        senderAccount.get().getBalance() == SENDER_INITIAL_BALANCE - transferAmount
+        senderAccount.get().getBalance() == TransferControllerSpec.SENDER_INITIAL_BALANCE - transferAmount
     }
 
     def "Should add transfer amount to receiver after a successful transaction"() {
@@ -81,10 +82,62 @@ class TransferControllerSpec extends Specification {
         def requestBody = createRequestBody(senderAccountId, receiverAccountId, transferAmount)
 
         when:
-        client.toBlocking().retrieve(HttpRequest.POST(TRANSFER_URL, requestBody))
+        client.toBlocking().retrieve(HttpRequest.POST(TransferControllerSpec.TRANSFER_URL, requestBody))
 
         then:
-        receiverAccount.get().getBalance() == RECEIVER_INITIAL_BALANCE + transferAmount
+        receiverAccount.get().getBalance() == TransferControllerSpec.RECEIVER_INITIAL_BALANCE + transferAmount
+    }
+
+    def "Summed account balance should remain same after a successful transaction"() {
+        given:
+        def senderAccount = accountRepository.find(senderAccountId) as Optional<Account>
+        def receiverAccount = accountRepository.find(receiverAccountId) as Optional<Account>
+        def summedAmount = senderAccount.get().getBalance() + receiverAccount.get().getBalance()
+        def transferAmount = BigDecimal.ONE
+        def requestBody = createRequestBody(senderAccountId, receiverAccountId, transferAmount)
+
+        when:
+        client.toBlocking().retrieve(HttpRequest.POST(TransferControllerSpec.TRANSFER_URL, requestBody))
+        senderAccount = accountRepository.find(senderAccountId) as Optional<Account>
+        receiverAccount = accountRepository.find(receiverAccountId) as Optional<Account>
+        def summedAmountAfterTransfer = senderAccount.get().getBalance() + receiverAccount.get().getBalance()
+
+        then:
+        summedAmount == summedAmountAfterTransfer
+    }
+
+    def "Should throw exception response when transfer request amount is higher that sender balance"() {
+        given:
+        def transferAmount = TransferControllerSpec.SENDER_INITIAL_BALANCE + BigDecimal.ONE
+        def requestBody = createRequestBody(senderAccountId, receiverAccountId, transferAmount)
+
+        when:
+        client.toBlocking().retrieve(HttpRequest.POST(TransferControllerSpec.TRANSFER_URL, requestBody))
+
+        then:
+        thrown HttpClientResponseException
+    }
+
+    def "Should throw exception when sender account cannot be found"() {
+        given:
+        def requestBody = createRequestBody(0L, receiverAccountId, BigDecimal.ONE)
+
+        when:
+        client.toBlocking().retrieve(HttpRequest.POST(TransferControllerSpec.TRANSFER_URL, requestBody))
+
+        then:
+        thrown HttpClientResponseException
+    }
+
+    def "Should throw exception when receiver account cannot be found"() {
+        given:
+        def requestBody = createRequestBody(senderAccountId, 0L, BigDecimal.ONE)
+
+        when:
+        client.toBlocking().retrieve(HttpRequest.POST(TransferControllerSpec.TRANSFER_URL, requestBody))
+
+        then:
+        thrown HttpClientResponseException
     }
 
     def createRequestBody(long testSenderAccountId, long tesReceiverAccountId, BigDecimal testAmount) {
