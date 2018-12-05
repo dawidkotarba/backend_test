b12286245145f2645f29237b6924916240423b78
dawidkotarba <dawidkotarba>
2018-12-05 17:04:48 +0100

    Update controller test

--- src/test/groovy/com/dawidkotarba/backendtest/controller/TransferControllerSpec.groovy
+++ src/test/groovy/com/dawidkotarba/backendtest/controller/TransferControllerSpec.groovy
@@ -10,14 +10,17 @@ import io.micronaut.http.HttpRequest
 import io.micronaut.http.client.HttpClient
 import io.micronaut.inject.qualifiers.Qualifiers
 import io.micronaut.runtime.server.EmbeddedServer
-import org.junit.BeforeClass
+import org.junit.Before
 import spock.lang.AutoCleanup
 import spock.lang.Shared
 import spock.lang.Specification
 
 class TransferControllerSpec extends Specification {
 
-    def FIRST_TRANSFER_REQUEST_SEQUENCE_ID = 1L
+    final static def TRANSFER_URL = "/api/transfer"
+    final static def FIRST_TRANSFER_REQUEST_SEQUENCE_ID = 1L
+    final static def SENDER_INITIAL_BALANCE = BigDecimal.TEN
+    final  def RECEIVER_INITIAL_BALANCE = BigDecimal.ONE
 
     @Shared
     @AutoCleanup
@@ -30,22 +33,58 @@ class TransferControllerSpec extends Specification {
     @Shared
     def accountRepository = embeddedServer.applicationContext.getBean(Repository, Qualifiers.byName("accountRepository"))
 
-    @BeforeClass
+    @Shared
+    Long senderAccountId
+
+    @Shared
+    Long receiverAccountId
+
+    @Before
     def setupAccounts() {
-        accountRepository.save(new Account().withBalance(BigDecimal.TEN))
-        accountRepository.save(new Account().withBalance(BigDecimal.ONE))
+        def senderAccount = accountRepository.save(new Account().withBalance(SENDER_INITIAL_BALANCE))
+        senderAccountId = senderAccount.getId()
+
+        def receiverAccount = accountRepository.save(new Account().withBalance(RECEIVER_INITIAL_BALANCE))
+        receiverAccountId = receiverAccount.getId()
     }
 
     def "Should return a transfer request ID for a successful transaction"() {
         given:
-        def requestBody = createRequestBody(1L, 2L, BigDecimal.TEN)
+        def requestBody = createRequestBody(senderAccountId, receiverAccountId, BigDecimal.TEN)
 
         when:
-        def response = client.toBlocking().retrieve(HttpRequest.POST("/api/transfer", requestBody));
+        def response = client.toBlocking().retrieve(HttpRequest.POST(TRANSFER_URL, requestBody))
         def parsedResponse = new JsonSlurper().parseText(response)
         def responseDto = new TransferResponseDto(parsedResponse)
+
         then:
         responseDto.getTransferId() == FIRST_TRANSFER_REQUEST_SEQUENCE_ID
+    }
+
+    def "Should subtract transfer amount from sender after a successful transaction"() {
+        given:
+        def senderAccount = accountRepository.find(senderAccountId) as Optional<Account>
+        def transferAmount = BigDecimal.ONE
+        def requestBody = createRequestBody(senderAccountId, receiverAccountId, transferAmount)
+
+        when:
+        client.toBlocking().retrieve(HttpRequest.POST(TRANSFER_URL, requestBody))
+
+        then:
+        senderAccount.get().getBalance() == SENDER_INITIAL_BALANCE - transferAmount
+    }
+
+    def "Should add transfer amount to receiver after a successful transaction"() {
+        given:
+        def receiverAccount = accountRepository.find(receiverAccountId) as Optional<Account>
+        def transferAmount = BigDecimal.ONE
+        def requestBody = createRequestBody(senderAccountId, receiverAccountId, transferAmount)
+
+        when:
+        client.toBlocking().retrieve(HttpRequest.POST(TRANSFER_URL, requestBody))
+
+        then:
+        receiverAccount.get().getBalance() == RECEIVER_INITIAL_BALANCE + transferAmount
     }
 
     def createRequestBody(long testSenderAccountId, long tesReceiverAccountId, BigDecimal testAmount) {
